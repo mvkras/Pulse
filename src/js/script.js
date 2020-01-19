4cd31db1f1698ca918d677aad9e4fbb7fd86f3af
Andrew <avkras1992@gmail.com>
2020-01-19 18:43:23 +1000

    Изучаем JavaScript

---
+++ src/js/script.js
@@ -0,0 +1,32 @@
+/* alert('ты пидор');
+confirm('ты пидор?'); */ /* команда с вопросом */
+/* Создаем переменную */
+/* let answer = prompt('Ты пидор?',"");
+console.log(answer); */
+
+/* циклы */
+/* for(let i=1; i<12; i++){
+    console.log('Значение равно: '+i*2);
+}
+let number=12;
+if (number==8){
+    console.log('Верно, значение равно: '+number);
+}
+else{
+    console.log('You are Din-don!'); 
+}
+/* Функция и ее название */
+function login(){
+    console.log(123);
+}
+/* Вызов функции по названию */
+login();
+
+function userName(a,b){
+    console.log(a+b);
+}
+userName('Ivan ','Drago');
+userName('Dmitriy ',404);
+
+
+
