09ebca9dd9ee4afc40aa8c1a2a99cc833829725a
Andrew <avkras1992@gmail.com>
2020-01-19 22:31:19 +1000

    Делаем слайдер, начинаем альтернативные пути создания слайдера

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,21 @@
-/* alert('ты пидор');
-confirm('ты пидор?'); */ /* команда с вопросом */
-/* Создаем переменную */
-/* let answer = prompt('Ты пидор?',"");
-console.log(answer); */
+$(document).ready(function(){  /* функция, которая запускает слайдер, скачали с сайта Slick */
+    $('.carousel__inner').slick({
+        speed: 1200, /* Как быстро будет переключаться 1 слайд на другой в милисекундах. 1500 ms - это 1,5 секунды */
+        slidesToShow: 1,
+        /* adaptiveHeight: true, */    /* Самостоятельно будет подстраиваться под высоту */  
+        autoplay: true,  /* Автопереключение слайдера */
+        prevArrow:'<button type="button" class="slick-prev"><img src="../img/pictures/arrow_right.png"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="../img/pictures/arrow_left.png"></button>',
+        responsive: [
+            {
+                breakpoint: 768, /* на какой промежутке будем устанавливать эти правила работают вниз эти правила будут работать от 0 до 1024px */
+                settings: {                
+                  dots: true, 
+                  arrows: false  /* для мобильной версии стрелки убираем */
+            }
+         }] /* адаптация под мобилки, разное разрешение слайдов */  
+      }); /* Слайдер загружается тогда, когда слайдер полностью готов */ 
+      autoplaySpeed: 2000
+  });  /* Этот метод позволит нам запустить slick слайдер */
 
-/* циклы */
-/* for(let i=1; i<12; i++){
-    console.log('Значение равно: '+i*2);
-}
-let number=12;
-if (number==8){
-    console.log('Верно, значение равно: '+number);
-}
-else{
-    console.log('You are Din-don!'); 
-}
-/* Функция и ее название */
-function login(){
-    console.log(123);
-}
-/* Вызов функции по названию */
-login();
-
-function userName(a,b){
-    console.log(a+b);
-}
-userName('Ivan ','Drago');
-userName('Dmitriy ',404);
-
-
-
+  
